62a602f8208cb3870114526a8242576b8642d55c
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-23 11:46:09 -0500

    Set up all necessary requires

---
+++ app.js
@@ -0,0 +1,9 @@
+  const bodyParser = require('body-parser');
+  const express = require('express');
+  const mongodb = require('mongodb');
+  const mongoose = require('mongoose');
+  mongoose.Promise = require('bluebird');
+  const mustache = require('mustache');
+  const mustacheEpxress = require('mustache-express');
+
+  mongoose.connect('mongodb://localhost:27017/disneyPlushies');
